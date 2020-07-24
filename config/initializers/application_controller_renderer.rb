259c0def450bf6b4d2bc84d3d9e88d6f3b44c921
chiba432 <chibako714@gmail.com>
2020-07-24 15:35:36 +0900

    Add

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8dfbe4b8b6b63ed95fe328fa6108fea67932b0c93409760cb43f1d0a23a87faf46e0c500f68001591442434a83c00434c593e0c25bd1ef739b98a5c223a6045e'
